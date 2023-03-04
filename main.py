8ff4212ecacc9c853a6c33da8da137f7770ba7a6
Elad Levi <58475124+JakePeralta7@users.noreply.github.com>
2023-03-04 20:57:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,32 @@ import datetime
 import time
 import requests
 import platform
+import multiprocessing
+
+
+class MetricsCollector(multiprocessing.Process):
+    def __init__(self):
+        super(MetricsCollector, self).__init__()
+
+    def run(self):
+        send_event()
+        time.sleep(1)
 
 
 def send_event(event: dict):
     event_url = "http://ubuntu-cyber.elad.net:5000/api/event"
-    host = platform.node()
-    operating_system = platform.system()
+    my_system = platform.uname()
+    host = my_system.node
+    operating_system = my_system.system
+    os_release = my_system.release
+    os_version = my_system.version
+    machine = my_system.machine
+    processor = my_system.processor
     current_datetime = time.mktime(datetime.datetime.now().timetuple())
     a = requests.post(url=event_url, json={'time': current_datetime, 'host': host, 'operating_system': operating_system,
-                                           'event': event})
+                                           'os_release': os_release, 'os_version': os_version, 'machine': machine,
+                                           'processor': processor, 'event': event})
+    print(a)
 
 
 def main():
